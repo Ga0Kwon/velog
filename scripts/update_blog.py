e1a8c7e873789a6432b9bd0b9202f5d5aa3908f4
Ga0Kwon <121651318+Ga0Kwon@users.noreply.github.com>
2024-02-29 09:55:28 +0900

    Update update_blog.py

--- scripts/update_blog.py
+++ scripts/update_blog.py
@@ -3,8 +3,8 @@ import git
 import os
 
 # 벨로그 RSS 피드 URL
-# example : rss_url = 'https://api.velog.io/rss/@gayeong39'
-rss_url = 'https://api.velog.io/rss/@gayeong39'
+# example : rss_url = 'https://v2.velog.io/rss/gayeong39'
+rss_url = 'https://v2.velog.io/rss/gayeong39'
 # 깃허브 레포지토리 경로
 repo_path = '.'
 
